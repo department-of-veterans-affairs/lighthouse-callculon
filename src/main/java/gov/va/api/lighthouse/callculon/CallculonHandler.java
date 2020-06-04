2bd94f9473c1e6c5555e2f97b0c2e106ea920c89
Bryan Schofield <bryan.schofield@libertyits.com>
2020-06-04 17:08:49 -0400

    .

--- src/main/java/gov/va/api/lighthouse/callculon/CallculonHandler.java
+++ src/main/java/gov/va/api/lighthouse/callculon/CallculonHandler.java
@@ -2,16 +2,14 @@ package gov.va.api.lighthouse.callculon;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import java.time.Instant;
 
-public class CallculonHandler implements RequestHandler<ScheduledEvent, CallculonResponse> {
+public class CallculonHandler implements RequestHandler<String, CallculonResponse> {
 
   @Override
-  public CallculonResponse handleRequest(ScheduledEvent scheduledEvent, Context context) {
-    context.getLogger().log("OH MAN GALRIC! " + scheduledEvent + " " + context);
-    context.getLogger().log(scheduledEvent.getDetailType());
-    context.getLogger().log("" + scheduledEvent.getDetail());
+  public CallculonResponse handleRequest(String event, Context context) {
+    context.getLogger().log("OH MAN GALRIC! " + context.getAwsRequestId());
+    context.getLogger().log(event);
 
     return CallculonResponse.builder()
         .epochTime(Instant.now().toEpochMilli())
