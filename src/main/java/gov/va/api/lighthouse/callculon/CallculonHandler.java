39373176bcf463a771b8200f01fd705ef98f196f
Bryan Schofield <bryan.schofield@libertyits.com>
2020-06-04 11:50:48 -0400

    .

--- src/main/java/gov/va/api/lighthouse/callculon/CallculonHandler.java
+++ src/main/java/gov/va/api/lighthouse/callculon/CallculonHandler.java
@@ -1,8 +1,14 @@
 package gov.va.api.lighthouse.callculon;
 
-public class CallculonHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import java.time.Instant;
 
-  public void deleteMe() {
-    System.out.println("delete me");
+public class CallculonHandler implements RequestHandler<ScheduledEvent, CallculonResponse> {
+
+  @Override
+  public CallculonResponse handleRequest(ScheduledEvent scheduledEvent, Context context) {
+    return CallculonResponse.builder().timeStamp(Instant.now()).statusCode(418).build();
   }
 }
---
+++ src/main/java/gov/va/api/lighthouse/callculon/CallculonResponse.java
@@ -0,0 +1,14 @@
+package gov.va.api.lighthouse.callculon;
+
+import java.time.Instant;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CallculonResponse {
+
+  int statusCode;
+
+  Instant timeStamp;
+}
